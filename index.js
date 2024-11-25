eac2ed347dc57834d20397cae75dd3ebb0be7155
cxfl <fenglinggod@qq.com>
2024-11-26 02:58:05 +0800

    remake with local render instead of server render

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.getElementById('search-input');
-    const tagButtons = document.querySelectorAll('.tag-btn');
-    const bookmarks = document.querySelectorAll('.bookmark');
-    const bm_repo = document.body.dataset.reponame;
-    const bm_user = document.body.dataset.username;
-    const starButton = document.getElementById('star-button');
-    const starIcon = starButton.querySelector('.star-icon');
+    const limit_labels = 20;
 
     let selectedTags = new Set();
     let debounceTimer;
@@ -21,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function filterBookmarks() {
+        const bookmarks = document.querySelectorAll('.bookmark');
         const searchTerm = searchInput.value.toLowerCase();
 
         bookmarks.forEach(bookmark => {
@@ -51,12 +47,16 @@ document.addEventListener('DOMContentLoaded', () => {
         filterBookmarks();
     }
 
-    tagButtons.forEach(button => {
-        button.addEventListener('touchstart', handleTouchStart);
-        button.addEventListener('touchmove', handleTouchMove);
-        button.addEventListener('touchend', handleTouchEnd);
-        button.addEventListener('click', handleClick);
-    });
+    // Move the tag button event listener setup to a separate function
+    function attachTagEventListeners() {
+        const tagButtons = document.querySelectorAll('.tag-btn');
+        tagButtons.forEach(button => {
+            button.addEventListener('touchstart', handleTouchStart);
+            button.addEventListener('touchmove', handleTouchMove);
+            button.addEventListener('touchend', handleTouchEnd);
+            button.addEventListener('click', handleClick);
+        });
+    }
 
     function handleTouchStart(e) {
         e.currentTarget.touchStartTime = Date.now();
@@ -81,27 +81,24 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    filterBookmarks();
-
-    function showTokenDialog() {
+    function showDialog() {
         const dialog = document.createElement('div');
         dialog.innerHTML = `
             <div class="dialog-overlay"></div>
             <div class="dialog-content">
-                <h2>设置 GitHub 个人访问令牌</h2>
+                <h2>设置书签仓库信息</h2>
                 <p>请输入您的 GitHub 个人访问令牌。这将用于访问 GitHub API。</p>
                 <input type="password" id="github-token-input" placeholder="输入您的 GitHub 令牌">
+                <p>请输入您的 GitHub 书签仓库。</p>
+                <input type="text" id="github-repo-input" placeholder="输入您的 GitHub 书签仓库: e.g. fenglingback/TDC">
                 <button id="save-token-btn">保存</button>
-                <button id="cancel-btn">&times;</button>
             </div>
         `;
         document.body.appendChild(dialog);
         document.body.style.overflow = 'hidden';
 
         const saveButton = document.getElementById('save-token-btn');
-        const cancelButton = document.getElementById('cancel-btn');
-        saveButton.addEventListener('click', checkAndSaveGitHubToken);
-        cancelButton.addEventListener('click', closeDialog);
+        saveButton.addEventListener('click', checkAndSaveInfo);
     }
 
     function closeDialog() {
@@ -109,11 +106,13 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = '';
     }
 
-    async function checkAndSaveGitHubToken() {
+    async function checkAndSaveInfo() {
         const tokenInput = document.getElementById('github-token-input');
+        const repoInput = document.getElementById('github-repo-input');
         const token = tokenInput.value.trim();
+        const repo = repoInput.value.trim();
 
-        const url = `https://api.github.com/user`;
+        const url = `https://api.github.com/repos/${repo}`;
         const headers = {
             'Authorization': `token ${token}`,
             'Accept': 'application/vnd.github.v3+json'
@@ -122,69 +121,163 @@ document.addEventListener('DOMContentLoaded', () => {
         try {
             const response = await fetch(url, { method: 'GET', headers });
 
-            if (response.status === 200) {
+            if (response.ok) {
                 const data = await response.json();
-                if (data.login === bm_user) {
+                if (data.permissions.admin === true) {
                     localStorage.setItem('githubToken', token);
+                    localStorage.setItem('bmRepo', repo);
                     closeDialog();
-                    alert('令牌设置成功！');
+                    main();
                 } else {
-                    alert('抱歉，你没有权限部署！');
-                    tokenInput.value = '';
+                    alert('您不是该仓库的管理员！');
                 }
-            } else {
-                alert('令牌无效，请检查令牌是否正确');
-                tokenInput.value = '';
             }
         } catch (error) {
-            alert('检查网络连接！');
+            alert('检查网络连接和输入信息是否正确！');
             console.error('Error:', error);
         }
     }
 
-    async function toggleStar() {
-        const token = localStorage.getItem('githubToken');
-        if (!token) {
-            showTokenDialog();
-            return;
+    // 获取所有issues
+    async function getAllIssues(repo, headers) {
+
+        const issues_url = `https://api.github.com/repos/${repo}/issues`;
+
+        let page = 1;
+        let issues = [];
+        while (true) {
+            const response = await fetch(`${issues_url}?page=${page}&per_page=100&state=open`, {
+                headers: headers
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            if (data.length === 0) {
+                break;
+            }
+
+            issues = issues.concat(data);
+            page++;
         }
 
-        const url = `https://api.github.com/user/starred/${bm_user}/${bm_repo}`;
-        const headers = {
-            'Authorization': `token ${token}`,
-            'Accept': 'application/vnd.github.v3+json'
-        };
+        const issues_data = issues.map(issue => {
+            let [url, desc] = [];
+            if (issue.body.includes('\n\n')) {
+                [url, desc] = issue.body.split('\n\n');
+            } else if (issue.body.includes('\r\n\r\n')) {
+                [url, desc] = issue.body.split('\r\n\r\n');
+            } else {
+                [url, desc] = [issue.body, '']
+            }
+
+
+            return {
+                title: issue.title,
+                url: url,
+                desc: desc,
+                labels: issue.labels.map(label => label.name)
+            };
+        })
+
+        // console.log(issues_data);
+        return issues_data;
+    }
+
+
+    // 提取标签
+    function extractLabels(issues) {
+        const labelCount = new Map();
+        issues.forEach(issue => {
+            if (issue.labels.length > 0) {
+                issue.labels.forEach(label => {
+                    if (labelCount.has(label)) {
+                        labelCount.set(label, labelCount.get(label) + 1);
+                    } else {
+                        labelCount.set(label, 1);
+                    }
+                });
+            }
+        });
+
+        const sortedLabels = new Map([...labelCount.entries()].sort((a, b) => b[1] - a[1]));
+        // console.log(sortedLabels);
+
+        const labels = Array.from(sortedLabels.keys());
+        if (labels.length <= limit_labels) {
+            return labels;
+        } else {
+            new_labels = [];
+            issues.forEach(issue => {
+                if (issue.labels.length > 0) {
+                    issue.labels.sort((a, b) => sortedLabels.get(b) - sortedLabels.get(a));
+                    // console.log(issue.labels);
+                    for (const label of issue.labels) {
+                        if (new_labels.includes(label)) {
+                            continue;
+                        } else {
+                            new_labels.push(label);
+                            break;
+                        }
+                    }
+                };
+            })
+            new_labels.sort((a, b) => sortedLabels.get(b) - sortedLabels.get(a));
+            return new_labels
+        }
+    }
+
+
+    // 渲染页面
+    function renderPage(issues_data, labels) {
+        const tagsContainer = document.getElementById('tags-container');
+        for (const label of labels) {
+            tagsContainer.innerHTML += `<button class="tag-btn" data-tag="${label}">${label}</button>`;
+        }
+        const bookmarksContainer = document.getElementById('bookmarks-container');
+        for (const issue of issues_data) {
+            bookmarksContainer.innerHTML += `
+                <a href="${issue.url}" target="_blank" class="bookmark" data-tags="${issue.labels.join(',')}" rel="noopener noreferrer">
+                    <span class="bookmark-title">${issue.title}</span>
+                    <div class="bookmark-desc">${issue.desc}</div>
+                    <div class="bookmark-tags"><span>${issue.labels.join('</span><span>')}</span></div>
+                </a>`;
+        }
+
+        // Add this line to attach event listeners after rendering
+        attachTagEventListeners();
+    }
+
+
+    async function main() {
 
         try {
-            const starResponse = await fetch(url, { method: 'PUT', headers });
-
-            if (starResponse.status === 204) {
-                starButton.classList.add('starred');
-                starIcon.setAttribute('fill', 'currentColor');
-                console.log('Successfully starred the repository');
-
-                setTimeout(async () => {
-                    const unstarResponse = await fetch(url, { method: 'DELETE', headers });
-
-                    if (unstarResponse.status === 204) {
-                        starButton.classList.remove('starred');
-                        starIcon.setAttribute('fill', 'none');
-                        console.log('Successfully unstarred the repository');
-                        alert('正在启动部署，部署时间平均30s，部署完成后刷新页面即可');
-                    } else {
-                        console.error('Failed to unstar the repository');
-                    }
-                }, 1000);
-            } else {
-                console.error('Failed to star the repository');
+            if (!localStorage.getItem('githubToken') || !localStorage.getItem('bmRepo')) {
+                showDialog();
+                return;
             }
+
+            console.log("\n %c TDC %c https://github.com/fenglingback/TDC \n", "color: #0ca7a4; background-image: linear-gradient(90deg, #555555 0%, #555555 100%); padding:5px 1px;", "color: #a277ff; background-image: linear-gradient(90deg, #555555 0%, #1d294d 100%); padding:5px 0;");
+
+            const repo = localStorage.getItem('bmRepo');
+            const token = localStorage.getItem('githubToken');
+            const headers = {
+                'Authorization': `token ${token}`,
+                'Accept': 'application/vnd.github.v3+json'
+            };
+
+            const issues = await getAllIssues(repo, headers);
+            const labels = await extractLabels(issues);
+            renderPage(issues, labels);
+
+            filterBookmarks();
         } catch (error) {
-            alert('部署失败，打开控制台查看报错，检查你的token和网络连接，如果仍然无法解决，请联系网站管理员');
-            console.error('Error:', error);
+            console.error(error);
         }
+
     }
 
-    starButton.addEventListener('click', toggleStar);
-
-    console.log("\n %c TDC %c https://github.com/fenglingback/TDC \n", "color: #0ca7a4; background-image: linear-gradient(90deg, #555555 0%, #555555 100%); padding:5px 1px;", "color: #a277ff; background-image: linear-gradient(90deg, #555555 0%, #1d294d 100%); padding:5px 0;");
+    main();
 });
