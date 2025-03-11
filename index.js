bcb57869c26a2626e7c1a51e8cf131178dddc5f4
cxfl <fenglinggod@qq.com>
2025-03-11 09:10:22 +0800

    A trivial optimization

--- index.js
+++ index.js
@@ -407,7 +407,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const addBookmarkBtn = document.getElementById("add-bookmark-btn")
 
-    addBookmarkBtn.addEventListener("click", () => {
+    handleClick(addBookmarkBtn, (e) => {
         if (!localStorage.getItem("githubToken") || !localStorage.getItem("bmRepo")) {
             showDialog()
             return
@@ -459,10 +459,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         function checkTagsListDisplay() {
+            // 如果所有标签都被隐藏，则隐藏标签列表
             if (Array.from(tags).filter((tag) => tag.style.display !== "none").length === 0) {
                 changeTagsListDisplay(false)
             } else {
                 changeTagsListDisplay(true)
+                tagsInput.focus()
             }
         }
 
